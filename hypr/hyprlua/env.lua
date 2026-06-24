44128c1266736d4fd186a0c8d9bf04701a584ea5
BharathBala <bharathchandra92251@gmail.com>
2026-06-24 17:48:08 +0530

    feat: add environment variables for desktop session and toolkit backend

--- hypr/hyprlua/env.lua
+++ hypr/hyprlua/env.lua
@@ -9,7 +9,17 @@ browser = "zen-browser"
 
 hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/hyprshot/")
 
--- Themes
+--XDF
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Toolkit Backend Variables
+hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+
+
+--Qt Variables
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "kde")
-hl.env("XDG_MENU_PREFIX", "plasma-")
