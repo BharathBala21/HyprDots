af1958d6c33123ed2b8171743d15dda3aaf7384e
BharathBala21 <bharathchandra92251@gmail.com>
2026-08-05 15:30:59 +0530

    zoom pinch

--- hypr/hyprlua/input.lua
+++ hypr/hyprlua/input.lua
@@ -10,3 +10,7 @@ hl.config({
 hl.gesture({ fingers = 4, direction = "down", action = function() hl.dispatch(hl.dsp.focus({ workspace = "r-1" })) end })
 hl.gesture({ fingers = 4, direction = "up", action = function() hl.dispatch(hl.dsp.focus({ workspace = "r+1" })) end })
 
+hl.gesture({ fingers = 3, direction = "swipe", action = "scroll_move" })
+-- hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 2 })
+-- hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1.2, mode = "mult" })
+hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1, mode = "live" })
