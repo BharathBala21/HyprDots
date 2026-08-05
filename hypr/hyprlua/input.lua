4d81c2f98f97a928864337cc34670eb8c9d4e5a8
BharathBala21 <bharathchandra92251@gmail.com>
2026-08-05 15:25:26 +0530

    workspace swipe with trackpad

--- hypr/hyprlua/input.lua
+++ hypr/hyprlua/input.lua
@@ -1,9 +1,12 @@
-
 hl.config({
-
-    input =  {
-		touchpad = {
-			natural_scroll = true
-		}
-	},
+    input = {
+        touchpad = {
+            natural_scroll = true
+        }
+    },
 })
+
+-- 4-finger vertical swipe gestures for workspace switching
+hl.gesture({ fingers = 4, direction = "down", action = function() hl.dispatch(hl.dsp.focus({ workspace = "r-1" })) end })
+hl.gesture({ fingers = 4, direction = "up", action = function() hl.dispatch(hl.dsp.focus({ workspace = "r+1" })) end })
+
