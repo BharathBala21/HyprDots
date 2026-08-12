300bb190ad17e66fc2d73f1cac353a1bed3498cd
BharathBala21 <bharathchandra92251@gmail.com>
2026-08-12 21:39:50 +0530

    fix: update follow_min_visible to improve scrolling behavior

--- hypr/hyprlua/layout.lua
+++ hypr/hyprlua/layout.lua
@@ -10,7 +10,7 @@ hl.config({
     scrolling = {
         direction = "right",
         focus_fit_method = 1,
-        follow_min_visible = 0.4,
+        follow_min_visible = 1,
         explicit_column_widths = "0.333, 0.5, 0.667, 1.0",
         fullscreen_on_one_column = true,
     }
