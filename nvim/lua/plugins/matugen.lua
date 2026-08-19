583a40ac245e739c8c898c03379acec9762d55c4
BharathBala21 <bharathchandra92251@gmail.com>
2026-08-19 19:13:42 +0530

    add nvim config

---
+++ nvim/lua/plugins/matugen.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "Senal-D-A-Gunaratna/matugen.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      load_theme = true,
+      palette_path = "~/.cache/matugen/nvim-colors.json",
+    },
+  },
+}
---
+++ nvim/lua/plugins/transparent.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      extra_groups = {
+        "NormalFloat",
+        "FloatBorder",
+        "NvimTreeNormal",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "TelescopeNormal",
+        "TelescopeBorder",
+        "WhichKeyFloat",
+        "LineNr",
+        "CursorLineNr",
+        "SignColumn",
+        "StatusLine",
+        "StatusLineNC",
+      },
+    },
+  },
+}
